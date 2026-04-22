d99c2a0fee32f0fc0930eb53de5fd03cd61c5a6a
ilga <kurmanaliev.ilgiz92@gmail.com>
2026-04-22 13:04:29 +0600

    fix: restore login in BaseTest

--- src/test/java/ui/tests/BaseTest.java
+++ src/test/java/ui/tests/BaseTest.java
@@ -4,6 +4,7 @@ import framework.driver.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import ui.pages.LoginPage;
 
 public abstract class BaseTest {
 
@@ -13,12 +14,16 @@ public abstract class BaseTest {
     public void setUp() {
         DriverFactory.initDriver();
         driver = DriverFactory.getDriver();
+
         driver.get("https://www.saucedemo.com/");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "secret_sauce");
+        loginPage.waitForInventoryPageToLoad();
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        
         DriverFactory.quitDriver();
     }
 }
